3db5c7e79323de80e93331830fbc6a3c58b8263b
dominic9074 <dominickb098@gmail.com>
2026-09-01 15:58:31 +0530

    feat:added the navbar component to the page

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,8 +1,11 @@
-
+import Navbar from "../components/navbar"
 
 export default function Blogs(){
     return (
+        <>
+        <Navbar />
         <h2>BlogList page</h2>
+        </>
     )
 }
 
